2e0a4e3db5c260305c638df5ce1847ccefeac06e
ShahdEbrehem <shahdebrehem@gmail.com>
2025-08-03 02:13:54 +0300

    shahdebrehem

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -9,7 +9,16 @@
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <a class="btn btn-primary btn-sm" href="{{ route('notes.index') }}"><i class="fa fa-arrow-left"></i> Back</a>
         </div>
-
+                 @if ($errors->any())
+                <div class="alert alert-danger">
+                <strong>Whoops!</strong> في أخطاء لازم تصلحيها:<br><br>
+                <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+                </ul>
+                </div>
+                @endif
         <form action="{{ route('notes.update',$note->id) }}" method="POST">
             @csrf
             @method('PUT')
@@ -17,28 +26,28 @@
             <div class="mb-3">
                 <label for="inputTitle" class="form-label"><strong>Title:</strong></label>
                 <input 
-                    type="text" 
-                    name="Title" 
-                    value="{{ $note->title }}"
-                    class="form-control @error('Title') is-invalid @enderror" 
-                    id="inputTitle" 
-                    placeholder="Title">
-                @error('Title')
-                    <div class="form-text text-danger">{{ $message }}</div>
-                @enderror
+             type="text" 
+             name="title" 
+             value="{{ old('title', $note->title) }}"
+             class="form-control @error('title') is-invalid @enderror" 
+             id="inputTitle" 
+             placeholder="Title">
+             @error('title')
+            <div class="form-text text-danger">{{ $message }}</div>
+              @enderror
             </div>
 
             <div class="mb-3">
                 <label for="inputcontent" class="form-label"><strong>content:</strong></label>
                 <textarea 
-                    class="form-control @error('content') is-invalid @enderror" 
-                    style="height:150px" 
-                    name="content" 
-                    id="inputcontent" 
-                    placeholder="content">{{ $note->content }}</textarea>
-                @error('content')
-                    <div class="form-text text-danger">{{ $message }}</div>
-                @enderror
+                class="form-control @error('content') is-invalid @enderror" 
+                 style="height:150px" 
+                 name="content" 
+                 id="inputcontent" 
+                  placeholder="Content">{{ old('content', $note->content) }}</textarea>
+                  @error('content')
+                  <div class="form-text text-danger">{{ $message }}</div>
+                   @enderror
             </div>
             <button type="submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Update</button>
         </form>
